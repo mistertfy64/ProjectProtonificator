f0ab5b9290b8ae0f740205c744cd1c018d27f7de
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-25 09:13:36 +0700

    feature: "add" formatNumber function

---
+++ js/format-number.js
@@ -0,0 +1,11 @@
+const SUFFIXES = ["", "k", "M", "B", "T", "q", "Q", "s", "S", "O", "N"]; // and so on...
+
+/**
+ * Formats the number into something more human-readable.
+ * @param x The number to format (should be of `Decimal` instance)
+ */
+function formatNumber(x) {
+	// const logarithm = x.log10().floor();
+	// const powerToDivideBy = new Decimal(10).pow(logarithm.div(3).floor());
+	// const digits = x.div(powerToDivideBy).toFixed(3);
+}
