25050eb3fc7d7635fc77605bb9a87a3b76e80c25
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-25 08:43:43 +0700

    change: added proper event implementation for touch devices + refactoring

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@ const game = getNewGameData();
 
 function initialize() {
 	// TODO: add saving/loading
+	initializeEvents();
+}
+
+/**
+ * Initializes the events for the HTML elements.
+ */
+function initializeEvents() {
+	$("#generator--particles").on("mousedown", () => {
+		setParticleButtonState(true);
+	});
+	$("#generator--particles").on("mouseup", () => {
+		setParticleButtonState(false);
+	});
+	$("#generator--particles").on("touchstart", () => {
+		setParticleButtonState(true);
+	});
+	$("#generator--particles").on("touchend", () => {
+		setParticleButtonState(false);
+	});
 }
 
 initialize();
