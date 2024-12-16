e34265826c1805dcd0e763c058af1e3fa0cb1191
mistertfy64 <mistertfy64@mistertfy64.com>
2024-12-16 15:43:12 +0700

    feature: added saving system (no saving in localstorage yet :frown:)

---
+++ js/saving.js
@@ -0,0 +1,34 @@
+function exportSaveData() {
+	const data = JSON.stringify(game);
+	const message =
+		"This is your save data. Make sure to save this text in somewhere safe!";
+	prompt(message, data);
+}
+
+function importSaveData() {
+	const importedData = JSON.parse(prompt("Enter your save data here."));
+	const importedDataKeys = keyify(importedData);
+	const initialData = getNewGameData();
+	const initialDataKeys = new Set(keyify(initialData));
+	for (const key of importedDataKeys) {
+		if (_.get(initialData, key) instanceof Decimal) {
+			_.set(game, key, new Decimal(_.get(importedData, key)));
+			continue;
+		}
+		if (!initialDataKeys.has(key)) {
+			continue;
+		}
+		_.set(game, key, _.get(importedData, key));
+	}
+}
+
+// Taken from https://stackoverflow.com/a/47063174/
+const keyify = (obj, prefix = "") =>
+	Object.keys(obj).reduce((res, el) => {
+		if (Array.isArray(obj[el])) {
+			return res;
+		} else if (typeof obj[el] === "object" && obj[el] !== null) {
+			return [...res, ...keyify(obj[el], prefix + el + ".")];
+		}
+		return [...res, prefix + el];
+	}, []);
